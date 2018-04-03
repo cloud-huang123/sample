a826ade0e72e0dd89c3676bc4beb486fb856d0d5
cloud-huang <1206802428@qq.com>
2018-04-03 15:07:27 +0000

    youhua

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Sample App</title>
-</head>
-<body>
-	<h1>我要去学php做网站啦，现在要学C++ ，PHP， 小程序开发，感觉好爆炸</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title','主页')
+@section('content')
+	<div class="jumbotron">
+		<h1>这是一个标题</h1>
+		<p class="lead">
+			你现在看到的就是简单登录注册页面
+		</p>
+		<p>
+			一切，将从这里开始。
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+		</p>
+	</div>
+@stop
